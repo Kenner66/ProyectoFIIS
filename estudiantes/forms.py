8d22681561c0d406a706ed66ff450bbb859deea5
Kenner66 <kennerechevarria@gmail.com>
2024-09-27 19:18:28 -0500

    estudiantes pt3

--- estudiantes/forms.py
+++ estudiantes/forms.py
@@ -4,12 +4,14 @@ from usuarios.models import Usuario
 class EstudianteForm(forms.ModelForm):
     class Meta:
         model = Estudiante
-        fields = ['usuario', 'codigo','base', 'carrera','activo']
+        fields = ['usuario', 'codigo', 'base', 'carrera', 'activo']
 
     def __init__(self, *args, **kwargs):
         super(EstudianteForm, self).__init__(*args, **kwargs)
-        # Filtra para mostrar solo los usuarios que tienen rol de "Estudiante"
-        self.fields['usuario'].queryset = Usuario.objects.filter(rol__nombre_rol="Estudiante")
+        # Filtra para mostrar solo los usuarios que no están ya registrados como estudiantes
+        estudiantes = Estudiante.objects.values_list('usuario_id', flat=True)
+        self.fields['usuario'].queryset = Usuario.objects.filter(rol__nombre_rol="Estudiante").exclude(id__in=estudiantes)
+
 
 class InformacionPersonalForm(forms.ModelForm):
     class Meta:
